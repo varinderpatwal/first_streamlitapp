8cf95f6a41e2778ac272c09e2dc4c22a3884da3f
varinderpatwal <47208219+varinderpatwal@users.noreply.github.com>
2023-05-26 18:34:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-echo "Running line no 38"
+print "Running line no 38"
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
