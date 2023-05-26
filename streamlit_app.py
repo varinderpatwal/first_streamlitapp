bb9caf5ef3a81a07631fdbcf1c53db305a3af8b2
varinderpatwal <47208219+varinderpatwal@users.noreply.github.com>
2023-05-26 17:43:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-import snowflake.connector
+
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -34,6 +34,6 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-
+import snowflake.connector
 
 
