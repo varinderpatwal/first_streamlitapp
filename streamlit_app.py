891835e971edaebee790fbbaaac2e421edef8138
varinderpatwal <47208219+varinderpatwal@users.noreply.github.com>
2023-05-26 11:25:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
